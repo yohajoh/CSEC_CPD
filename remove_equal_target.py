a6fb18d87c492afe4033441565991ad4b23421a4
yohannes belete <ybelete490@gmail.com>
2025-11-19 07:54:28 -0500

    v7 commit

--- remove_equal_target.py
+++ remove_equal_target.py
@@ -38,36 +38,36 @@
 #         print(nums)
 # Solution().moveZeroes([1,2])
 
-# class Solution:
-#     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-#         l, r = 0, len(numbers) - 1
-#         while l < r:
-#             sum = numbers[l]+numbers[r]
-#             if sum == target:
-#                 return [l+1, r+1]
-#             elif sum < target:
-#                 l += 1
-#             else: 
-#                 r -= 1
-#         return []
-print(Solution().twoSum([2,3,4], 6))
 class Solution:
-    def maxArea(self, height: list[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        best = 0
-        while left < right:
-            h = min(height[left], height[right])
-            w = right - left
-            area = h * w
-            if area > best:
-                best = area
-            if height[left] < height[right]:
-                left += 1
-            else:
-                right -= 1
-        return best              
-print(Solution().maxArea([1,1]))
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            sum = numbers[l]+numbers[r]
+            if sum == target:
+                return [l+1, r+1]
+            elif sum < target:
+                l += 1
+            else: 
+                r -= 1
+        return []
+print(Solution().twoSum([2,3,4], 6))
+# class Solution:
+#     def maxArea(self, height: list[int]) -> int:
+#         left = 0
+#         right = len(height) - 1
+#         best = 0
+#         while left < right:
+#             h = min(height[left], height[right])
+#             w = right - left
+#             area = h * w
+#             if area > best:
+#                 best = area
+#             if height[left] < height[right]:
+#                 left += 1
+#             else:
+#                 right -= 1
+#         return best              
+# print(Solution().maxArea([1,1]))
 
 # class Solution:
 #     def removeDuplicates(self, nums: list[int]) -> int:
